c0a7475c72424fb0ca80a9b542dad55cee5d56f3
Michael Park <michael160313@gmail.com>
2022-07-29 22:30:51 +0900

    first commit

---
+++ Test01/src/Math/Gugudan.java
@@ -0,0 +1,17 @@
+package Math;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=9;i++) {
+			System.out.println(i+" 단을 출력합니다.\n");
+				
+				for(int j=1;j<=9;j++) {
+					System.out.println(i+" x "+j+" = "+i*j+"\n");
+				}
+				System.out.println("\n");
+		}
+
+	}
+
+}
