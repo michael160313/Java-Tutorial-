c2b1398b9a51b4c33b2141e31b8cf9e656d23159
Michael Park <michael160313@gmail.com>
2022-08-08 21:48:52 +0900

    remove .git files

---
+++ CodingTest/src/Baekjoon/AplusB.java
@@ -0,0 +1,20 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class AplusB {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = 0;
+		int b = 0;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a + b);
+	}
+
+}
